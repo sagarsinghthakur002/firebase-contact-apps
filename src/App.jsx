a1cb5b2ab500b5c590528f99e84b7fec981a2c4e
sagarsinghthakur002 <sagarsinghthakur002@gmail.com>
2024-10-11 16:15:01 +0545

    feat: import firebase

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='text-red-700'>App</div>
   )
 }
 
---
+++ src/config/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBy3_q4QfPayCQM_6us-OOMIC00RdXT5AQ",
+  authDomain: "fir-contact-app-5a286.firebaseapp.com",
+  projectId: "fir-contact-app-5a286",
+  storageBucket: "fir-contact-app-5a286.appspot.com",
+  messagingSenderId: "160087274432",
+  appId: "1:160087274432:web:037ec106f7805203aa4e25"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
