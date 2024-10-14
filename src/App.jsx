9a74e0ec8e0614aa44a531389ecf3050ce29ba1f
sagarsinghthakur002 <sagarsinghthakur002@gmail.com>
2024-10-14 10:22:47 +0545

    feat: first page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import { IoIosSearch } from "react-icons/io";
+import { MdAddToPhotos } from "react-icons/md";
 const App = () => {
   return (
-    <div className='text-red-700'>
+    <div className='text-red-700 mx-auto max-w-[370px] px-4 '>
+
       <Navbar />
+    <div className='flex gap-2'>
+      <div className='flex relative items-center flex-grow'>
+      <IoIosSearch className=' absolute text-white  text-3xl' />
+
+        <input className='bg-transparent border-white border focus: h-10 rounded-md flex-grow text-white px-8'
+         type='text'  />
+      </div>
+      <MdAddToPhotos  className='text-4xl text-slate-800 '/>
+    </div> 
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='bg-white  flex justify-center h-[64px] m-4 items-center rounded-[20px]'>
+    <div className='bg-white  flex justify-center h-[64px]  items-center rounded-[10px] my-5'>
         <img className='h-[20px]' src='/public/image/logo.png' alt='' />
         <h1 className='font-bold font-serif'>Contact App</h1>
     </div>
